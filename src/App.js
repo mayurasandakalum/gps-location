1f6efe7f5e173f6488fd445ac6760461f2426c7f
Mayura Sandakalum <mssellapperuma@gmail.com>
2023-02-18 13:04:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import useGeoLocation from "./hooks/useGeoLocation";
 
 function App() {
+  const location = useGeoLocation();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {location.loaded
+        ? JSON.stringify(location)
+        : "Location data not available yet."}
     </div>
   );
 }
